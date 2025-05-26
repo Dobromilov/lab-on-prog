de167b83a313c31e08ff7017f2644322419e6d2c
Dobromilov <144216509+Dobromilov@users.noreply.github.com>
2025-05-26 22:43:15 +0700

    Delete RGZ/5 directory

--- RGZ/5/mylib.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYLIB_H
-#define MYLIB_H
-
-void encrypt(const char* input, char* output, int rows, int cols);
-void decrypt(const char* input, char* output, int rows, int cols);
-int verify(const char* a, const char* b);
-
-#endif
